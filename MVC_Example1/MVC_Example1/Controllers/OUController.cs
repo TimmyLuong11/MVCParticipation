6dc6dfe7e7ea6d099f2691af6cbe3c93b1ed3ef6
TimmyLuong11 <Timothy.luong@ou.edu>
2021-11-16 16:08:17 -0600

    Finished Example

---
+++ MVC_Example1/MVC_Example1/Controllers/OUController.cs
@@ -0,0 +1,33 @@
+﻿using MVC_Example1.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace MVC_Example1.Controllers
+{
+    public class OUController : Controller
+    {
+        // GET: OU
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult TAs()
+        {
+            List<Student> tas = new List<Student>()
+            {
+                new Student()
+                {
+                    FirstName = "Timmy",
+                    LastName = "Luong",
+                    SoonerID = 1
+
+                }
+            };
+            return View(tas);
+        }
+    }
+}
---
+++ MVC_Example1/MVC_Example1/Models/Student.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Web;
+
+namespace MVC_Example1.Models
+{
+    public class Student
+    {
+        public string FirstName { get; set; }
+        [DisplayName("TA LastName")]
+        public string LastName { get; set; }
+        public int SoonerID { get; set; }
+
+    }
+}
